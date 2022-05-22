a961f6b1749240377e3da9e37bc187e816ff6d9e
Justin <justin27sackprasit@gmail.com>
2022-05-22 13:14:09 +1000

    Create function to add books to an array
    The values of the input elements are cleared after a book object is
    pushed into the myLibrary array. There are no data validation checks for
    the inputs yet.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+const submitButton = document.querySelector('.submit');
+submitButton.addEventListener('click', addBookToLibrary);
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,5 +10,30 @@ function Book(title, author, pages, read) {
   this.read = read;
   this.info = function() {
     return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}.`;
+  }
+}
+
+function addBookToLibrary() {
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let readStatusY = document.querySelector('#read');
+  let readStatusN = document.querySelector('#not_read');
+  let readStatus = true;
+  if (title === '' || author === '' || pages === '' || (!readStatusY.checked && !readStatusN.checked)) {
+    alert('Please complete the form.');
+  } else {
+    if (readStatusY.checked) {
+      readStatus = true;
+    } else {
+      readStatus = false;
+    }
+    let book = new Book(title, author, pages, readStatus);
+    myLibrary.push(book);
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    document.querySelector('#read').checked = false;
+    document.querySelector('#not_read').checked = false;
   }
 }
