e75ec594c6f9f193967b0c6f5d0bbc0a8379d6d8
Justin <justin27sackprasit@gmail.com>
2022-05-28 17:25:28 +1000

    Add function to display books in a card format
    Also added a function that clears the cards from the card-screen element
    which will be used to update the screen whenever a new book is added.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let myLibrary = [];
 
+let book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 300, true);
+let book2 = new Book('Skulduggery Pleasant', 'Derek Landy', 200, true);
+let book3 = new Book('American Gods', 'Neil Gaiman', 600, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+
 const submitButton = document.querySelector('.submit');
 submitButton.addEventListener('click', addBookToLibrary);
 
@@ -30,6 +37,7 @@ function addBookToLibrary() {
     }
     let book = new Book(title, author, pages, readStatus);
     myLibrary.push(book);
+    // Clear input values
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
     document.querySelector('#pages').value = '';
@@ -37,3 +45,35 @@ function addBookToLibrary() {
     document.querySelector('#not_read').checked = false;
   }
 }
+
+let cardScreen = document.querySelector('.card-screen');
+
+function loadBooksDiplays() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let card = document.createElement('div');
+    let titleDisplay = document.createElement('div');
+    let authorDisplay = document.createElement('div');
+    let pagesDisplay = document.createElement('div');
+    let readDisplay = document.createElement('div');
+    titleDisplay.textContent = myLibrary[i].title;
+    authorDisplay.textContent = myLibrary[i].author;
+    pagesDisplay.textContent = `${myLibrary[i].pages} pages`;
+    readDisplay.textContent = myLibrary[i].read ? 'Read' : 'Not read';
+    card.appendChild(titleDisplay);
+    card.appendChild(authorDisplay);
+    card.appendChild(pagesDisplay);
+    card.appendChild(readDisplay);
+    card.classList.add('card');
+    cardScreen.appendChild(card);
+  }
+}
+
+function clearBooksDisplay() {
+  let card = cardScreen.lastElementChild;
+  while (card) {
+    cardScreen.removeChild(card);
+    card = cardScreen.lastElementChild;
+  }
+}
+
+loadBooksDiplays();
